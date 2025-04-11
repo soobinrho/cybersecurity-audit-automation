7fd8916551a39185bc6a2be2885d2d515fe027f5
Soobin Rho <soobinrho@gmail.com>
2025-04-11 12:08:25 -0500

    docs: UI component design plan for <main> and <aside>
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -66,9 +66,51 @@ export default async function DashboardPage() {
           <div className="min-h-svh min-w-xs w-full 2xl:max-w-[80%] border-x-[0.08rem] border-dashed">
             <main className="px-16 py-8">
               <aside>
-                [https://ui.shadcn.com/docs/components/accordion style side bar]
+                <p>[https://ui.shadcn.com/docs/components/sidebar]</p>
+                <p>
+                  Big screen behavior: No button for collapsing or opening the
+                  sidebar. It's always on.
+                </p>
+                <p>
+                  Small screen behavior: Button for collapsing or opening the
+                  sidebar. Floating variant.
+                </p>
+                <section>
+                  <h2>TODO: Items in the nav bar</h2>
+                  <p>
+                    1. The main page should be what the user wants the most.
+                    This kind of info should be located at the path of least
+                    resistance. Mode: How scadcn's website reads. Top to bottom
+                    and left to right design principle. Make it easy for user to
+                    follow the instructions, ideally in just one step.
+                  </p>
+
+                  <p>
+                    2. Include "Getting Started" page with super simple
+                    instructions. Think of how easy setting up Cloudflare Tunnel
+                    is. They give you a little box highlighting how to execute
+                    the script to do the tunneling. I think this should be the
+                    main page of the dashboard.
+                  </p>
+                  <p>
+                    3. Include "Manage" button at the "Getting Started" page.
+                    Allow the user to generate a new client-side script, which
+                    will remove the previous API key and populate the new
+                    client-side script with the new key.
+                  </p>
+
+                  <p>4. Include MFA, PITR, and RLS.</p>
+                </section>
               </aside>
-              <article>[Main content based on the page]</article>
+              <article>
+                <p>[Main content based on the page]</p>
+                <p>[https://ui.shadcn.com/docs/components/data-table]</p>
+
+                <p>
+                  Allow the user to delete any item, or edit, but not add.
+                  Adding should only be done via the client side Python program.
+                </p>
+              </article>
             </main>
             <footer className="px-16 py-8">
               <div>
