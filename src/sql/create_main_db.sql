02c7f6c2e79b830e2a9b89bf164813ef5585bdf1
Soobin Rho <soobinrho@gmail.com>
2025-04-11 12:08:25 -0500

    refactor: RFC5424 standard for logs
    Reference: https://datatracker.ietf.org/doc/html/rfc5424
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/sql/create_main_db.sql
+++ src/sql/create_main_db.sql
@@ -1,61 +1,65 @@
 -- This sql script creates main.db
 -- Store only what we need. Extra data = extra liability.
-CREATE TABLE Organizations (
-  OrganizationID TEXT PRIMARY KEY NOT NULL,
-  OrganizationName TEXT NOT NULL COLLATE NOCASE,
-  OrganizationURL TEXT NOT NULL
+CREATE TABLE organizations (
+  org_id TEXT PRIMARY KEY NOT NULL,
+  org_name TEXT NOT NULL COLLATE NOCASE,
+  org_url TEXT NOT NULL,
+  org_last_updated_on_caa TEXT NOT NULL
+                            DEFAULT (unixepoch('now','subsec'))
 );
 
-CREATE TABLE Projects (
-  ProjectID TEXT PRIMARY KEY NOT NULL,
-  OrganizationID_fk TEXT NOT NULL,
-  ProjectName TEXT NOT NULL COLLATE NOCASE,
-  IsPITREnabled INTEGER NOT NULL
-                CHECK (IsPITREnabled BETWEEN 0 and 1),
-  FOREIGN KEY(OrganizationID_fk) REFERENCES Organizations(OrganizationID)
+CREATE TABLE users (
+  user_id INTEGER PRIMARY KEY NOT NULL,
+  user_email TEXT NOT NULL COLLATE NOCASE
+             CHECK (user_email LIKE '%@%'),
+  user_is_mfa_enabled INTEGER NOT NULL
+                      CHECK (user_is_mfa_enabled BETWEEN 0 and 1)
 );
 
-CREATE TABLE Users (
-  UserID INTEGER PRIMARY KEY NOT NULL,
-  Email TEXT NOT NULL COLLATE NOCASE
-        CHECK (Email LIKE '%@%'),
-  IsMFAEnabled INTEGER NOT NULL
-               CHECK (IsMFAEnabled BETWEEN 0 and 1)
+CREATE TABLE projects (
+  project_id TEXT PRIMARY KEY NOT NULL,
+  org_id_fk TEXT NOT NULL,
+  project_name TEXT NOT NULL COLLATE NOCASE,
+  project_is_pitr_enabled INTEGER NOT NULL
+                          CHECK (project_is_pitr_enabled BETWEEN 0 and 1),
+  FOREIGN KEY(org_id_fk) REFERENCES organizations(org_id)
 );
 
-CREATE TABLE Tables (
-  TableID INTEGER PRIMARY KEY NOT NULL,
-  ProjectID_fk TEXT NOT NULL,
-  TableName TEXT NOT NULL COLLATE NOCASE,
-  IsRLSEnabled INTEGER NOT NULL
-               CHECK (IsRLSEnabled BETWEEN 0 and 1),
-  FOREIGN KEY(ProjectID_fk) REFERENCES Projects(ProjectID)
+CREATE TABLE tables (
+  table_id INTEGER PRIMARY KEY NOT NULL,
+  project_id_fk TEXT NOT NULL,
+  table_name TEXT NOT NULL COLLATE NOCASE,
+  table_is_rls_enabled INTEGER NOT NULL
+                       CHECK (table_is_rls_enabled BETWEEN 0 and 1),
+  FOREIGN KEY(project_id_fk) REFERENCES projects(project_id)
 );
 
-CREATE TABLE OrganizationsThatUsersAreIn (
-  OrganizationID_fk TEXT NOT NULL,
-  UserID_fk INTEGER NOT NULL,
-  UserRoleInOrganization TEXT NOT NULL,
-  PRIMARY KEY (OrganizationID_fk, UserID_fk),
-  FOREIGN KEY(OrganizationID_fk) REFERENCES Organizations(OrganizationID),
-  FOREIGN KEY(UserID_fk) REFERENCES Users(UserID)
+CREATE TABLE organization_members (
+  org_id_fk TEXT NOT NULL,
+  user_id_fk INTEGER NOT NULL,
+  org_member_role TEXT NOT NULL,
+  PRIMARY KEY (org_id_fk, user_id_fk),
+  FOREIGN KEY(org_id_fk) REFERENCES organizations(org_id),
+  FOREIGN KEY(user_id_fk) REFERENCES users(user_id)
 );
 
-CREATE TABLE Logs (
-  LogID INTEGER PRIMARY KEY NOT NULL,
-  OrganizationID_fk TEXT,
-  UserID_fk INTEGER,
-  ProjectID_fk TEXT,
-  TableID_fk INTEGER,
-  TimeRecorded INTEGER NOT NULL DEFAULT (unixepoch('now','subsec')),
-  IsComplianceRelated INTEGER NOT NULL
-                      CHECK (IsComplianceRelated BETWEEN 0 and 1),
-  ComplianceStatus TEXT NOT NULL COLLATE NOCASE
-                   CHECK (ComplianceStatus IN ('Pass', 'Fail', 'Not Applicable')),
-  LogMessage TEXT NOT NULL COLLATE NOCASE,
-  FOREIGN KEY(OrganizationID_fk) REFERENCES Organizations(OrganizationID),
-  FOREIGN KEY(UserID_fk) REFERENCES Users(UserID),
-  FOREIGN KEY(ProjectID_fk) REFERENCES Projects(ProjectID),
-  FOREIGN KEY(TableID_fk) REFERENCES Tables(TableID)
+CREATE TABLE logs (
+  log_id INTEGER PRIMARY KEY NOT NULL,
+  org_id_fk TEXT,
+  user_id_fk INTEGER,
+  project_id_fk TEXT,
+  table_id_fk INTEGER,
+  PRI INTEGER NOT NULL,
+  VER INTEGER NOT NULL,
+  TIMESTAMP INTEGER NOT NULL
+            DEFAULT (unixepoch('now','subsec')),
+  HOSTNAME TEXT NOT NULL COLLATE NOCASE,
+  APPNAME TEXT NOT NULL COLLATE NOCASE,
+  PROCID TEXT NOT NULL COLLATE NOCASE,
+  STRUCTURED_DATA TEXT NOT NULL,
+  FOREIGN KEY(org_id_fk) REFERENCES organizations(org_id),
+  FOREIGN KEY(user_id_fk) REFERENCES users(user_id),
+  FOREIGN KEY(project_id_fk) REFERENCES projects(project_id),
+  FOREIGN KEY(table_id_fk) REFERENCES tables(table_id)
 );
 
