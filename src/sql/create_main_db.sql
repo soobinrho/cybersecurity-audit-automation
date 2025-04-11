8a4cc08615438acbed71ebcb5afaea5177039add
Soobin Rho <soobinrho@gmail.com>
2025-04-11 12:08:25 -0500

    refactor: better representation of primary keys
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/sql/create_main_db.sql
+++ src/sql/create_main_db.sql
@@ -3,17 +3,16 @@
 CREATE TABLE organizations (
   org_id TEXT PRIMARY KEY NOT NULL,
   org_name TEXT NOT NULL COLLATE NOCASE,
-  org_url TEXT NOT NULL,
   org_last_updated_on_caa TEXT NOT NULL
                             DEFAULT (unixepoch('now','subsec'))
 );
 
 CREATE TABLE users (
-  user_id INTEGER PRIMARY KEY NOT NULL,
-  user_email TEXT NOT NULL COLLATE NOCASE
-             CHECK (user_email LIKE '%@%'),
+  user_email TEXT PRIMARY KEY NOT NULL COLLATE NOCASE,
   user_is_mfa_enabled INTEGER NOT NULL
-                      CHECK (user_is_mfa_enabled BETWEEN 0 and 1)
+                      CHECK (user_is_mfa_enabled BETWEEN 0 and 1),
+  user_last_updated_on_caa TEXT NOT NULL
+                             DEFAULT (unixepoch('now','subsec'))
 );
 
 CREATE TABLE projects (
@@ -22,33 +21,37 @@ CREATE TABLE projects (
   project_name TEXT NOT NULL COLLATE NOCASE,
   project_is_pitr_enabled INTEGER NOT NULL
                           CHECK (project_is_pitr_enabled BETWEEN 0 and 1),
+  project_last_updated_on_caa TEXT NOT NULL
+                                DEFAULT (unixepoch('now','subsec')),
   FOREIGN KEY(org_id_fk) REFERENCES organizations(org_id)
 );
 
 CREATE TABLE tables (
-  table_id INTEGER PRIMARY KEY NOT NULL,
   project_id_fk TEXT NOT NULL,
-  table_name TEXT NOT NULL COLLATE NOCASE,
+  table_name INTEGER NOT NULL COLLATE NOCASE,
   table_is_rls_enabled INTEGER NOT NULL
                        CHECK (table_is_rls_enabled BETWEEN 0 and 1),
+  table_last_updated_on_caa TEXT NOT NULL
+                              DEFAULT (unixepoch('now','subsec')),
+  PRIMARY KEY (project_id_fk, table_name),
   FOREIGN KEY(project_id_fk) REFERENCES projects(project_id)
 );
 
 CREATE TABLE organization_members (
   org_id_fk TEXT NOT NULL,
-  user_id_fk INTEGER NOT NULL,
+  user_email_fk TEXT NOT NULL,
   org_member_role TEXT NOT NULL,
-  PRIMARY KEY (org_id_fk, user_id_fk),
+  PRIMARY KEY (org_id_fk, user_email_fk),
   FOREIGN KEY(org_id_fk) REFERENCES organizations(org_id),
-  FOREIGN KEY(user_id_fk) REFERENCES users(user_id)
+  FOREIGN KEY(user_email_fk) REFERENCES users(user_email)
 );
 
 CREATE TABLE logs (
   log_id INTEGER PRIMARY KEY NOT NULL,
   org_id_fk TEXT,
-  user_id_fk INTEGER,
+  user_email_fk TEXT,
   project_id_fk TEXT,
-  table_id_fk INTEGER,
+  table_name_fk TEXT,
   PRI INTEGER NOT NULL,
   VER INTEGER NOT NULL,
   TIMESTAMP INTEGER NOT NULL
@@ -58,8 +61,8 @@ CREATE TABLE logs (
   PROCID TEXT NOT NULL COLLATE NOCASE,
   STRUCTURED_DATA TEXT NOT NULL,
   FOREIGN KEY(org_id_fk) REFERENCES organizations(org_id),
-  FOREIGN KEY(user_id_fk) REFERENCES users(user_id),
+  FOREIGN KEY(user_email_fk) REFERENCES users(user_email),
   FOREIGN KEY(project_id_fk) REFERENCES projects(project_id),
-  FOREIGN KEY(table_id_fk) REFERENCES tables(table_id)
+  FOREIGN KEY(table_name_fk) REFERENCES tables(table_name)
 );
 
